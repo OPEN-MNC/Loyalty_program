a11f67b57aa258209a489dbb50f3c0b93692c96a
Amit Barai <studenta299@gmail.com>
2023-08-09 21:10:44 +0530

    first commit

---
+++ lib/customer/nav_bar.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class nav_bar_routes extends StatefulWidget {
+  const nav_bar_routes({super.key});
+
+  @override
+  State<nav_bar_routes> createState() => _nav_bar_routesState();
+}
+
+class _nav_bar_routesState extends State<nav_bar_routes> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
